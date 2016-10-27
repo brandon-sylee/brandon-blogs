c52c98301e0816f0f58d7ba8176050d35ba4d417
brandonLee <dd.stay.hf@gmail.com>
2016-10-27 18:04:32 +0900

    update readme

--- readme.cmd
+++ readme.cmd
@@ -1,8 +1,11 @@
-Spring Boot with Pebble
-React / React-dom / Bebel
-Facebook
+# Spring Boot, Pebble, React, Webpack
+
+## how to build
+$ webpack
+Ctrl + F9
 
 
-
-
-reference site : http://mitchellbosecke.com/pebble/home
+## reference site:
+  - http://mitchellbosecke.com/pebble/home
+  - https://github.com/velopert/react-skeleton
+  - https://facebook.github.io/react/
